d7f932ac2de583658e33b319b28570cbd36060f0
Richard van Berkum - Dunique - <rbe01@Walbeekgroep.local>
2016-03-08 17:16:24 +0100

    css added to format

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
 $(function(){
-
+    $(".btn-below").each(
+        function(i, button){
+            var width = $(button).width();
+            console.log(width);
+            $(button).css("margin-left", -width/2 + "px");
+        }
+    )
 });
 
