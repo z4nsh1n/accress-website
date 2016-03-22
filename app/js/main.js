ac502846c21268a408c2a5ddd544c700766e952f
Richard van Berkum - Dunique - <rbe01@Walbeekgroep.local>
2016-03-22 14:43:39 +0100

    Added slider

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,14 @@ $(function(){
             console.log(width);
             $(button).css("margin-left",-width/2 + "px");
         }
-    )
+    );
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        items: 1,
+        autoplay: true,
+        smartSpeed: 400,
+    });
 });
 
